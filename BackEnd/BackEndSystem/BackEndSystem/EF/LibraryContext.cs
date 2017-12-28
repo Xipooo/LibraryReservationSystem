73a51914fbcadca527bbc276c9bf7624b2bd4c63
Enes Hanson <enes.hanson@Yahoo.com>
2017-12-28 12:39:42 -0800

    bdb

---
+++ BackEnd/BackEndSystem/BackEndSystem/EF/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndSystem.EF
+{
+    public class LibraryContext : DbContext
+    {
+        public DbSet<BookList> BookList { get; set; }
+        public DbSet<Genre> Genre { get; set; }
+        public DbSet<Author> Author { get; set; }
+        public DbSet<MediaType> MediaType { get; set; }
+    }
+}
