79b03ab117ffa7758ae7703e1c1fa11d86fd0f60
Marshall Frink <marshallfrink@gmail.com>
2017-12-29 13:34:25 -0800

    I updated HomePageLogo

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,1 +1,21 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+        <img src="http://image.ibb.co/ceTBHG/WaffleO.png" alt="WaffleO" border="0" />        <a href="Register_Page">Register</a>
+        <a href="Login_Page">Login</a>
+        <a href="Register_Page">Register</a>
+            </div>
+        );
+    }
+}
+ReactDOM.render(
+    <HomePage/>,
+    document.getElementById("root")
+)
